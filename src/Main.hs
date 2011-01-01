253353401301108112528aae2e324921a752737c
p.balzarek <p.balzarek@googlemail.com>
2011-01-01 22:27:26 +0100

    tabs initial
    Ignore-this: 4b1df543977cf9a907015d5cdb1ea92f
    
    darcs-hash:20110101212726-e94ff-63268a02e542fa1b4866bf1437928ca0df2c0a0d.gz

--- src/Main.hs
+++ src/Main.hs
@@ -17,12 +17,16 @@ import Data.Bits
 import Data.List
 import Data.Maybe
 
+import Data.List.PointedList.Circular as PL
+
 import Data.IORef
 
 import qualified Data.Map as M
 import System.Glib.Flags
 
--- import Web.HBrowser.SetProxy (setProxy) -- TODO: uncomment
+import qualified Web.HBrowser.WebViewInternals as Web
+-- import Web.HBrowser.SetProxy (setProxy) -- TODO: uncomment]
+import Web.HBrowser.ViewContainer
 
 type Keymap = M.Map KeyDef (WebInputMonad ())
 type ModifierFlags = Int 
@@ -30,22 +34,38 @@ type KeyDef = (ModifierFlags, GTK.KeyVal)
 type WebMonad = ReaderT WebConf IO
 type MapRef = IORef [Keymap]
 
-
-
 -- Design: The keymap IORef holds a stack (aka list) of keymaps, 
 -- a #submap# pushes one keymap on the top, so we can exit it by poping the map
 -- INVARIANT: the keymap stack should never be empty
 
 
 data WebConf = WebConf 
-  { keymap     :: MapRef
-  , view       :: Web.WebView
+  { mapRef     :: MapRef
+  , tabs       :: Tabs
+  , container  :: GTK.ScrolledWindow
   , defaultMap :: Keymap 
+  , showTabs   :: [Web.WebView] -> IO ()
   } 
 
 data NextMap = Reset | Keep | Back 
 type WebInputMonad = StateT NextMap WebMonad
 
+deleteTab = ask >>= \conf -> liftIO $ do
+  current <- currentView (tabs conf)
+  deleted <- PL.delete `fmap` readIORef (tabs conf)
+  case deleted of 
+    Nothing -> Web.webViewLoadUri current "about:blank"
+    Just x -> do
+      GTK.widgetDestroy current
+      writeIORef (tabs conf) x
+      return ()
+  updateView (showTabs conf) (container conf) (tabs conf)
+
+newTab url = ask >>= \conf -> liftIO $ do
+  webView <- Web.webViewNew
+  Web.webViewLoadUri webView url
+  modifyIORef (tabs conf) $ insertRight webView
+  updateView (showTabs conf) (container conf) (tabs conf)
 
 
 keepKeymap = put Keep
@@ -87,24 +107,26 @@ top _        = error "top on empty stack"
 io = liftIO
 
 --submap :: (MonadReader WebConf m, MonadIO m) => Keymap (WebMonad ()) -> m ()
-submap map = ask >>=( \(keymap -> ref ) -> io $ modifyIORef ref (push map))
+submap map = ask >>=( \(mapRef -> ref ) -> io $ modifyIORef ref (push map))
              >> put Keep
 
 setKeymap :: (MonadReader WebConf m, MonadIO m) => Keymap -> m ()
-setKeymap map =  ask >>= \(keymap -> ref) -> io $ writeIORef ref [map]
+setKeymap map =  ask >>= \(mapRef -> ref) -> io $ writeIORef ref [map]
 
-modifyKeymap f =  ask >>= \(keymap -> ref) -> io $ 
+modifyKeymap f =  ask >>= \(mapRef -> ref) -> io $ 
                     readIORef ref >>= writeIORef ref . f
   
 
 loadURL url = do
-  page <- asks view
+  page <- io . currentView =<< asks tabs
   io $ Web.webViewLoadUri page url
 
 myKeymap view bar = M.fromList
            [ ((0, GTK.keyFromName "l"), submap loadKeymap)
            , ((0, GTK.keyFromName "h"), io $ putStrLn "hello world" )
            , ((0, GTK.keyFromName "o"), io $ activateInputBar bar "http://"  )
+           , ((0, GTK.keyFromName "n"), newTab "http://tagesschau.de"  )
+           , ((0, GTK.keyFromName "x"), deleteTab )
            ]
 
 loadKeymap = M.fromList
@@ -129,22 +151,22 @@ deactivateInputBar bar view = do
   GTK.widgetGrabFocus view
 
 
-handleKey keymap log view mapref = do 
+handleKey webConf = do 
   keyVal <-  GTK.eventKeyVal
   mods <- fromFlags <$> GTK.eventModifier
-  keymap <- io $ top <$> readIORef mapref
+  keymap <- io $ top <$> readIORef (mapRef webConf)
   case M.lookup (mods, keyVal) (map `M.union` keymap) of 
-    Nothing -> GTK.eventKeyName >>= io . log >> return False
-    Just action -> let conf = WebConf mapref view keymap in do
-      (_, nextmap) <- io $ runReaderT (runStateT action Reset) conf
+    Nothing -> return False
+    Just action -> do
+      (_, nextmap) <- io $ runReaderT (runStateT action Reset) webConf
       case nextmap of
-        Reset -> io $ writeIORef mapref [keymap]
-        Back  -> io $ modifyIORef mapref pop
+        Reset -> io $ writeIORef (mapRef webConf) [keymap]
+        Back  -> io $ modifyIORef (mapRef webConf) pop
         Keep  -> return ()
       return True
   where
     map = M.fromList [ ((0,GTK.keyFromName "q") , resetMap) ]
-    resetMap = setKeymap keymap
+    resetMap = setKeymap (defaultMap webConf)
 
 handleEntryKey bar view = do 
   keyName <-  GTK.eventKeyName
@@ -176,6 +198,28 @@ statusBarUpdate' hovering webView update = do
                   , uri]
   return ()
 
+
+(<$$>) = fmap.fmap
+
+notebookGrabCurrentPage notebook =
+  GTK.notebookGetCurrentPage notebook >>=
+  GTK.notebookGetNthPage notebook
+  
+getCurrentWebView tabs =  do
+  scrolledWindow <-  notebookGrabCurrentPage tabs
+  case scrolledWindow of 
+    Just x  -> Web.castToWebView <$$> GTK.binGetChild (GTK.castToBin x)
+    Nothing -> return Nothing
+  
+-- createWebView
+--   :: Maybe [Char] -> IO (Web.WebView, GTK.ScrolledWindow)
+-- createWebView url = do
+--   webView <- Web.webViewNew
+--   Web.webViewLoadUri webView (fromMaybe "about:blank" url)
+--   scrollWindow <- GTK.scrolledWindowNew Nothing Nothing
+--   GTK.containerAdd scrollWindow webView
+--   return (webView, scrollWindow)
+
 main = do
   GTK.initGUI
   
@@ -188,7 +232,10 @@ main = do
   urlBar <- GTK.entryNew
   GTK.entrySetHasFrame urlBar False
   
-  webView <- Web.webViewNew
+  tabLabel <- GTK.labelNew $ Just "()"
+  tabs <- containerNew
+  
+  webView <- currentView tabs
   
   hovering <- newIORef (Nothing, Nothing)
   
@@ -197,36 +244,38 @@ main = do
   
   statusbar <- GTK.labelNew Nothing
   GTK.set statusbar [ GTK.miscXalign  GTK.:= 0 ]
-
+  
   Web.webViewLoadUri webView "http://google.com"
   
   GTK.containerAdd window box
   
+  GTK.boxPackStart box tabLabel GTK.PackNatural 0
   GTK.boxPackStart box scrollWindow GTK.PackGrow    0
   GTK.boxPackStart box statusbar    GTK.PackNatural 0
   GTK.boxPackStart box urlBar       GTK.PackNatural 0  
   
   let log msg = GTK.labelSetText statusbar msg >> return ()
   let statusBarUpdate = statusBarUpdate' hovering webView log
-
-  let keymap = myKeymap webView urlBar 
+  let keymap = myKeymap tabs urlBar 
+  let showTabs = GTK.labelSetText tabLabel . show <=< titleList
       
   mapRef <- newIORef [keymap]
   
-  GTK.on webView GTK.keyPressEvent $ handleKey keymap putStrLn webView mapRef
+  webView' <- currentView tabs
+  
+  GTK.on scrollWindow GTK.keyPressEvent . handleKey $ WebConf mapRef tabs scrollWindow keymap showTabs
   GTK.on urlBar GTK.keyPressEvent $ handleEntryKey urlBar webView
   GTK.on webView Web.progressChanged . const $ statusBarUpdate
   
-  GTK.on webView Web.hoveringOverLink (\title url -> (writeIORef hovering (title, url) 
+  GTK.on webView' Web.hoveringOverLink (\title url -> (writeIORef hovering (title, url) 
                                                >> statusBarUpdate))
     
   GTK.on urlBar GTK.entryActivate $ do
     url <- GTK.entryGetText urlBar
     Web.webViewLoadUri webView url
-    deactivateInputBar urlBar webView
-  
+    deactivateInputBar urlBar scrollWindow  
   GTK.widgetShowAll window
 --   GTK.widgetHide urlBar
   GTK.onDestroy window GTK.mainQuit
-  deactivateInputBar urlBar webView
+  deactivateInputBar urlBar scrollWindow
   GTK.mainGUI
